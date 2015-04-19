47b3cfcbd5c17d34856fd2b720a9d7554c61251f
Weims14 <brendanweimer@gmail.com>
2015-04-19 12:59:31 -0500

    Made table references

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :category
-
+  has_many :line_items
+  has_many :orders, through: :line_items
   validates :name, presence: true, uniqueness: true
 end
